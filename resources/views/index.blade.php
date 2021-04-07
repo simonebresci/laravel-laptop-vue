d6011c6e0e496f31e6c87a76da3bcc297c6f4a54
unknown <simone.bresci.05@gmail.com>
2021-04-07 18:52:06 +0200

    vue with bug

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>Boolean Laravel </title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
+    
+
+</head>
+
+<body>
+
+    //Vue runs here
+    <div id="app"></div>
+
+</body>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
